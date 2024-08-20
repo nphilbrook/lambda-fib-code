429b0a3273d9f96fe4bb995efa3cec41521ae27d
nphilbrook <42616679+nphilbrook@users.noreply.github.com>
2024-08-20 13:25:40 -0400

    Update lambda_function.py

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -1,4 +1,26 @@
 # ---
 def lambda_handler(event, context):
-    """Add your function code here"""
-    return {"statusCode": 200, "body": f"Add your function code here"}
+    out = ""
+    if not 'body' in event or event['body'] is None:
+        out = "Post body required (must be an integer)"
+    else:
+        try:
+            fib = int(event['body'])
+            out = fibonacci(fib)
+        except ValueError:
+            out = "Post body must be an integer"
+    return {"statusCode": 200, "body": f"{out}"}
+
+def fibonacci(n):
+    """Iterative Fibonacci algorithm"""
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    previous = 0
+    last = 1
+    for i in range(2, n + 1):
+        acc = last + previous
+        previous = last
+        last = acc
+    return acc
