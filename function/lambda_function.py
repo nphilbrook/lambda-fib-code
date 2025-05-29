28ef600f85cb4cb3892e158710ce37b888f7932b
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-29 15:16:16 -0400

    trigger workflow

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -2,7 +2,7 @@
 def lambda_handler(event, context):
     out = ""
     if not 'body' in event or event['body'] is None:
-        out = "Post body required (must be an integer)"
+        out = "Post body required (must be an integer, fool!)"
     else:
         try:
             fib = int(event['body'])
