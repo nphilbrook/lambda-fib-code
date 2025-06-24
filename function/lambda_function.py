16d15a08e2beb95afa75f44ceb0b4edd660b8d18
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-06-24 09:18:07 -0400

    code update

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
             fib = int(event['body'])
             out = fibonacci(fib)
         except ValueError:
-            out = "Post body must be an integer, WRONG NAMESPACE"
+            out = "Post body must be an integer, FOOL"
     return {"statusCode": 200, "body": f"{out}"}
 
 def fibonacci(n):
